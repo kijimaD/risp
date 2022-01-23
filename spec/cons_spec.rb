3e83ee8df0d38ab7f3c13f4f8196462000701045
kijima <norimaking777@gmail.com>
2022-01-23 13:12:18 +0900

    test: add test

--- spec/cons_spec.rb
+++ spec/cons_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe Risp::Cons do
-  let(:cons) { Risp::Cons.new(:+, [1, 2]) }
+  let(:cons) do
+    Risp::Cons.new(:+,
+                   Risp::Cons.new(1,
+                                  Risp::Cons.new(2, :nil)))
+  end
 
   describe '#initialize' do
     it 'have car and cons attribute' do
       expect(cons.car).to eq :+
-      expect(cons.cdr).to eq [1, 2]
+      expect(cons.cdr.car).to eq 1
+      expect(cons.cdr.cdr.car).to eq 2
+      expect(cons.cdr.cdr.cdr).to eq :nil
     end
   end
 
@@ -25,7 +31,7 @@ RSpec.describe Risp::Cons do
       end
     end
 
-    context 'when exist other cons in cdr' do
+    context 'when cons nesting' do
       it 'return recursive evaluated value' do
         child = Risp::Cons.new(:+, [1, 2])
         parent = Risp::Cons.new(:+, [child, 2])
@@ -33,6 +39,51 @@ RSpec.describe Risp::Cons do
         env = Risp::Env.new(nil, { :+ => lambda {|x, y| x + y } })
         forms = Risp::Env.new(nil, { })
         expect(parent.lispeval(env, forms)).to eq 5
+      end
+    end
+  end
+
+  describe '#arrayify' do
+    context 'when conslist' do
+      it 'return array' do
+        expect(cons.arrayify).to eq [:+, 1, 2]
+      end
+    end
+
+    context 'when not conslist' do
+      it 'return original cons' do
+        cons = Risp::Cons.new(1, 2)
+        expect(cons.arrayify).to eq cons
+      end
+    end
+  end
+
+  describe '#conslist?' do
+    context 'when composing of cons' do
+      it 'return true' do
+        expect(cons.conslist?).to be true
+      end
+    end
+
+    context 'when containing other class' do
+      it 'return false' do
+        cons = Risp::Cons.new(1, 2)
+        expect(cons.conslist?).to be false
+      end
+    end
+  end
+
+  describe '#to_sexp' do
+    context 'when composing of cons' do
+      it 'return sexp' do
+        expect(cons.to_sexp).to eq '(+ 1 2)'
+      end
+    end
+
+    context 'when containing other class' do
+      it 'return cons added sexp' do
+        cons = Risp::Cons.new(:+, [1, 2])
+        expect(cons.to_sexp).to eq '(cons + (1 2))'
       end
     end
   end
